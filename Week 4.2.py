74fff884ae97dfbff5c02c21cfe2ad966af73082
Abdelrahman Mohamed <144355600+ab00d-m0@users.noreply.github.com>
2024-03-12 18:59:59 +0300

    Add files via upload

---
+++ Week 4.2.py
@@ -0,0 +1,25 @@
+class MySet:
+    def __init__(self):
+        self.elements = []
+    def add(self, x):
+        if x not in self.elements:
+            self.elements.append(x)
+            self.elements.sort()
+    def remove(self, x):
+        if x in self.elements:
+            self.elements.remove(x)
+    def contains(self, x):
+        return x in self.elements
+my_set = MySet()
+my_set.add(3)
+my_set.add(1)
+my_set.add(2)
+my_set.add(4)
+
+print("Set elements:", my_set.elements) 
+my_set.remove(3)
+print("Set elements:", my_set.elements)
+print("Does set contain 5?", my_set.contains(4))  
+print("Does set contain 3?", my_set.contains(3)) 
+
+# Abdelrahman Mohamed 11/314a
