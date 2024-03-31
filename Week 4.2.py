87cfd8ee07093bcc2b1bca2c634cc37df82ff330
Abdelrahman Mohamed <144355600+ab00d-m0@users.noreply.github.com>
2024-03-31 09:24:17 +0300

    Update Week 4.2.py

--- Week 4.2.py
+++ Week 4.2.py
@@ -1,15 +1,24 @@
 class MySet:
     def __init__(self):
         self.elements = []
+
     def add(self, x):
         if x not in self.elements:
             self.elements.append(x)
             self.elements.sort()
+
     def remove(self, x):
-        if x in self.elements:
-            self.elements.remove(x)
+        for i in range(len(self.elements)):
+            if self.elements[i] == x:
+                del self.elements[i]
+                break
+
     def contains(self, x):
-        return x in self.elements
+        for element in self.elements:
+            if element == x:
+                return True
+        return False
+
 my_set = MySet()
 my_set.add(3)
 my_set.add(1)
@@ -19,8 +28,9 @@ my_set.add(4)
 print("Set elements:", my_set.elements) 
 my_set.remove(3)
 print("Set elements:", my_set.elements)
-print("Does set contain 5?", my_set.contains(4))  
-print("Does set contain 3?", my_set.contains(3)) 
+print("Does set contain 4?", my_set.contains(4))  
+print("Does set contain 3?", my_set.contains(3))
+
 
 # Abdelrahman Mohamed 11/314a
 
