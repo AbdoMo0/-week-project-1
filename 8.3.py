f5b05c77209ef1426d261c0458f3810a06f0ed68
Abdelrahman Mohamed <144355600+ab00d-m0@users.noreply.github.com>
2024-04-29 15:48:11 +0300

    Add files via upload

---
+++ 8.3.py
@@ -0,0 +1,19 @@
+class Solution:
+    # @param A : list of integers
+    # @return a list of list of integers
+    def subsets(self, A):
+        A.sort()
+        result = []
+        self.generate_subsets(A, 0, [], result)
+        return result
+    def generate_subsets(self, A, index, current, result):
+        result.append(list(current))
+        for i in range(index, len(A)):
+            current.append(A[i])
+            self.generate_subsets(A, i + 1, current, result)
+            current.pop()
+
+A = [1, 2, 3]
+solution = Solution()
+output = solution.subsets(A)
+print(output)
