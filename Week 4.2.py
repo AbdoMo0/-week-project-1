855117f5ad2bfbbe876a145c8bd18850a8ce35fe
Abdelrahman Mohamed <144355600+ab00d-m0@users.noreply.github.com>
2024-05-01 04:32:10 +0300

    Update Week 4.2.py

--- Week 4.2.py
+++ Week 4.2.py
@@ -1,18 +1,17 @@
 class MySet:
     def __init__(self):
         self.elements = []
-
     def add(self, x):
-        if x not in self.elements:
-            self.elements.append(x)
-            self.elements.sort()
-
+        i = 0
+        while i < len(self.elements) and self.elements[i] < x:
+            i += 1
+        self.elements.insert(i, x)
     def remove(self, x):
-        for i in range(len(self.elements)):
-            if self.elements[i] == x:
-                del self.elements[i]
-                break
-
+        i = 0
+        while i < len(self.elements) and self.elements[i] != x:
+            i += 1
+        if i < len(self.elements) and self.elements[i] == x:
+            del self.elements[i]
     def contains(self, x):
         for element in self.elements:
             if element == x:
@@ -25,13 +24,10 @@ my_set.add(1)
 my_set.add(2)
 my_set.add(4)
 
-print("Set elements:", my_set.elements) 
-my_set.remove(3)
 print("Set elements:", my_set.elements)
-print("Does set contain 4?", my_set.contains(4))  
+my_set.remove(3)
+print("Set elements after removing 3:", my_set.elements)
+print("Does set contain 4?", my_set.contains(4))
 print("Does set contain 3?", my_set.contains(3))
-
-
-# Abdelrahman Mohamed 11/314a
 
 # you should use that the array is already sorted. please, rewrite a code without using ready operations as "remove" etc. you should go through the array using the loop
